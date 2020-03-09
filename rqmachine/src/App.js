f12aba86e83bcef2a6f50703bb3925f33c3db265
PdoubleU <piotrwitasik1988@gmail.com>
2020-03-09 12:12:12 +0100

    add div #conatiner, styles for #container

--- rqmachine/src/App.js
+++ rqmachine/src/App.js
@@ -4,19 +4,31 @@ import * as quotas from './quotas.js';
 
 function App() {
   return (
-    <div id="containter">
+    <div id="container">
       <div id="quote-box">
         <p id="text">" {quotas.QUOTAS_AND_AUTHORS[6][0]} "</p>
         <p id="author">{quotas.QUOTAS_AND_AUTHORS[6][1]}</p>
-        <span class="buttons">
-        <button id="new-quote" class="button">New quote</button>
-        <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank" class="button">
-          <i class="fa fa-twitter"></i>
+        <span className="buttons">
+        <button id="new-quote" className="button" color={this.state.color}>New quote</button>
+        <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank" className="button">
+          <i className="fa fa-twitter"></i>
         </a>
         </span>
       </div>
     </div>
   );
 }
+
+class changeColor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: ' ',
+      backgroundColor: ' ',
+    }
+  }
+}
+
 
 export default App;

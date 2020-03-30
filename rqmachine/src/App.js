fbe45be72aafa02772e74a633b5fc9cd2629dd3c
PdoubleU <piotrwitasik1988@gmail.com>
2020-03-30 13:40:59 +0200

    1. Add final project to github 2. Add fontawesome: twitter button and 'quota' icon. 3. Add import FontAwesomeIcons to App.js 4. Style icon 'twitt' and 'quota' icon 5. Remove quotation marks from quotas text - instead use 'quota' icon

--- rqmachine/src/App.js
+++ rqmachine/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.min.css';
 import * as quotas from './quotas.js';
 import ReactFCCtest from 'react-fcctest';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faQuoteRight } from  "@fortawesome/free-solid-svg-icons";
+
+
 
 class App extends React.Component{
   constructor(props) {
@@ -30,11 +35,12 @@ class App extends React.Component{
     return (
       <div className='container' style={this.state.backgroundColor}>
         <div id="quote-box" >
-          <p id="text" style={this.state.color}>" {this.state.text} " </p>
+          <p id="text" style={this.state.color}><span className="iconBox"><FontAwesomeIcon icon={faQuoteRight} className="icon"/></span>{this.state.text}</p>
           <p id="author" style={this.state.color}> - {this.state.author} </p>
           <span className="buttons">
           <button id="new-quote" className="button" onClick={this.handleClick} style={this.state.backgroundColor}>new quote</button>
           <a href={"https://twitter.com/intent/tweet?text=" + this.state.text + " - " + this.state.author} id="tweet-quote"  rel='noopener noreferrer' target="_blank" className="button" style={this.state.backgroundColor} data-size='large'>
+          <span className="twitterIcon"><FontAwesomeIcon icon={faTwitter} className='icon'/></span>
           </a>
           <ReactFCCtest/>
           </span>
